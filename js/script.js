65bf97ffbd917be3d827bce6a7b7965335b78010
Michael <myque@live.com>
2017-06-11 17:52:09 -0700

    finished major page

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+$(document).foundation();
+
+$("#griffin-canning").on("swipeleft",function(){
+  $(this).foundation('close');
+  $("#jake-bird").foundation('open');
+});
+
+$("#jake-bird").on("swiperight",function(){
+  $(this).foundation('close');
+  $("#griffin-canning").foundation('open');
+});
+
+$("#jake-bird").on("swipeleft",function(){
+  $(this).foundation('close');
+  $("#sean-brouchard").foundation('open');
+});
+
+$("#sean-brouchard").on("swiperight",function(){
+  $(this).foundation('close');
+  $("#jake-bird").foundation('open');
+});
+
 $(() => {
   $('.article-slider').lightSlider({
     loop: false,
